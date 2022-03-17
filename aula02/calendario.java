4601a46c42da8863e34d315b99de7070bfe6db93
Ana Chaves <93927006+anachavesrs@users.noreply.github.com>
2022-03-16 21:02:09 -0300

    Create calendario.java

---
+++ aula02/calendario.java
@@ -0,0 +1,15 @@
+public class calendario {
+    //função principal
+
+    public static void main(String[]args){
+        // mostrar uma mensagem para o usuário 
+        System.out.println("Digite o mês:");
+
+        // Ler um número digitado no teclado
+
+        // mostrar o nome do mês
+        //      usando if
+        //      usando switch
+    }
+    
+}
