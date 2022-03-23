382293a7b4614e785c727563ed0bed9ad87d55d5
Ana Chaves <annachav3s@gmail.com>
2022-03-23 00:14:11 -0300

    treinando

---
+++ treino/HoradoSistema.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class HoradoSistema {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é");
+        System.out.println(relogio.toString());
+    }
+    
+}
