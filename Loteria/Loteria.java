ef4812f404713f78c51ee0e1a9e9f936a07332ad
Ana Chaves <annachav3s@gmail.com>
2022-04-06 14:55:19 -0300

    .

---
+++ Loteria/Loteria.java
@@ -0,0 +1,10 @@
+public class Loteria {
+    
+    // Atributos
+    //Metodos, funçoes, proced...
+
+    //Rotina principal
+    public static void main(String[] args) {
+        System.out.println("Jogo Loteria feito em Java!");
+    }
+}
