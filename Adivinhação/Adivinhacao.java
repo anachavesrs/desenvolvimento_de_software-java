cca903f7d61880b12878770e5fa2f74c81359705
Ana Chaves <annachav3s@gmail.com>
2022-04-05 22:56:44 -0300

    commit full project

---
+++ Adivinhação/Adivinhacao.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Adivinhacao {
+    //Atributos
+    //Funções e proced
+
+    //Função principal
+    public static void main(String[] args) {
+        System.out.println("Jogo de adivinhação feito em Java!");
+        Random gerador = new Random();
+        int numSorteado = gerador.nextInt(100) + 1; // armazenando o n sorteado em uma var "numSorteado"
+        int vidas = 10; // var quantidade de vidas inicial
+        int palpite = 0;
+        Scanner sc = new Scanner(System.in); // habilitando entrada de dados
+
+        while (vidas > 0) {
+            System.out.println("Digite um número entre 1 e 100");
+            palpite = sc.nextInt();
+
+            if (palpite < 0 || palpite > 100) {
+                System.out.println("Numero inválido");
+                break;
+            }else{
+                if (palpite > numSorteado) {
+                    vidas --;
+                    System.out.println("O número escolhido é MAIOR que o número sorteado!");
+                    System.out.println("Vocêa ainda possui  " + vidas + " vidas");
+                } else if (palpite < numSorteado){
+                    vidas --;
+                    System.out.println("O número escolhido é MENOR que o número sorteado!");
+                    System.out.println("Vocêa ainda possui  " + vidas + " vidas");
+                }  else if (palpite == numSorteado){
+                    System.out.println("Parabéns!! Voce acertou!!");
+                    System.out.println("O número escolhido por voê foi: " + palpite + "e o número sorteado foi: " + numSorteado);
+                    break;
+                }          
+            }// fim else principal
+        }// fim while
+         if (vidas == 0) {
+             System.out.println("Voce perdeu! Suas vidas acabaram");
+         }
+
+        sc.close(); // fim da entrada de dados
+
+    }
+}// fim da classe
