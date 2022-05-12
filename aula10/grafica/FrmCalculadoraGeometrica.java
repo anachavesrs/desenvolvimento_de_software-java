ef86ebf8bca86a2d62332f7a42539fc621308f47
Ana Chaves <93927006+anachavesrs@users.noreply.github.com>
2022-05-11 22:01:33 -0300

    commit

--- aula10/grafica/FrmCalculadoraGeometrica.java
+++ aula10/grafica/FrmCalculadoraGeometrica.java
@@ -1,12 +1,25 @@
 package grafica;
 
-import java.awt.Dimension;
+import java.awt.*;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+
 
 public class FrmCalculadoraGeometrica extends JFrame {
     public FrmCalculadoraGeometrica(){
         setSize(new Dimension (720,600));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 1, 10, 10));
+        JPanel pnl = new JPanel();
+        pnl.setBorder(BorderFactory.createTitledBorder("Circulo"));
+        add(pnl);
+
+        pnl = new JPanel();
+        pnl.setBorder(BorderFactory.createTitledBorder("Retangulo"));
+        add(pnl);
+
+        pnl = new JPanel();
+        pnl.setBorder(BorderFactory.createTitledBorder("Triangulo"));
+        add(pnl);
     }
 }
--- aula10/logica/ProgramaPrincipal.java
+++ aula10/logica/ProgramaPrincipal.java
@@ -5,6 +5,7 @@ import grafica.FrmCalculadoraGeometrica;
 public class ProgramaPrincipal{
     public static void main(String[] args) {
         FrmCalculadoraGeometrica frm = new FrmCalculadoraGeometrica();
+    
         frm.setVisible(true);
     }
 }
