cee4469656b34bd3fc37cb58ee4c32e85fec414e
Ana Chaves <93927006+anachavesrs@users.noreply.github.com>
2022-05-18 19:37:44 -0300

    Create ActionCalcularArea.java

---
+++ aula10/grafica/ActionCalcularArea.java
@@ -0,0 +1,26 @@
+import javax.swing.AbstractAction;
+import javax.swing.JTextField;
+
+public class ActionCalcularArea extends AbstractAction{
+    private JTextField txtRaio;
+    private JTextField txtBase;
+    private JTextField txtAltura;
+
+    
+}
+
+private JTextField txtRaio;
+private JTextField txtBase;
+private JTextField txtAltura;
+private JTextField txtArea;
+private OpcaoForma opcaoForma;
+
+public ActionCalcularArea(JTextField txtRaio, opcaoForma opcaoForma, JTextField txtArea){
+    this.txtRaio = txtRaio;
+    this.opcaoForma = opcaoForma;
+}
+
+public ActionCalcularArea (JTextField txtBase, JTextField txtAltura, OpcaoForma opcaoForma, JTextField txtArea){
+    @Override
+    public Void actionPerformace(actionEvent)
+}
