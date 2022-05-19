a96403eddb310afb1f6d60dd2e81a0e6a302817d
Ana Chaves <93927006+anachavesrs@users.noreply.github.com>
2022-05-18 21:41:30 -0300

    commit

---
+++ aula11-calc-simples/grafica/FrmCalculadoraSimples.java
@@ -0,0 +1,12 @@
+package grafica;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class FrmCalculadoraSimples extends JFrame {
+    private JTextField txtX, txtY;
+    private JComboBox<String> cbxOperacao;
+    private JButton btnCalcular;
+}
