afb1072a74bdd883cbf4a037a89e53afe9cedb98
Ana Chaves <93927006+anachavesrs@users.noreply.github.com>
2022-05-25 19:48:35 -0300

    commit

---
+++ aula12/exemplo_exception/ProgramaPrincipal.java
@@ -0,0 +1,8 @@
+package exemplo_exception;
+
+public class ProgramaPrincipal {
+    public static void main(String[] args) {
+        int num = ConsleUtils.LerInteiro("Digite um inteiro: ");
+        System.out.printf("Inteiro digitado %d", num);
+    }
+}
